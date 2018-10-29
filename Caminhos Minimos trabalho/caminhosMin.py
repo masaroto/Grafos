c510dbd54f524da2b3738a7690559ced18ef752d
masaroto <pedrolandins@gmail.com>
2018-10-29 00:33:36 -0300

    trabalho final

--- Caminhos Minimos trabalho/caminhosMin.py
+++ Caminhos Minimos trabalho/caminhosMin.py
@@ -5,14 +5,37 @@ class Vertex:
 		self.dist = dist
 		self.pred = pred 
 		
-a = Vertex('a','NULL','NULL')
-b = Vertex('b','NULL','NULL')
-c = Vertex('c','NULL','NULL')
-d = Vertex('d','NULL','NULL')
-e = Vertex('e','NULL','NULL')
+# a = Vertex('a','NULL','NULL')
+# b = Vertex('b','NULL','NULL')
+# c = Vertex('c','NULL','NULL')
+# d = Vertex('d','NULL','NULL')
+# e = Vertex('e','NULL','NULL')
+# graph2 = {a:{b:10,c:3},b:{d:2,c:1},c:{b:4,d:8},d:{e:7},e:{d:9}}
+graph={}
+
+def criaVertice ():
+	aux = {}
+	nVertice = int(input("Digite o número de vertice: "))
+	for x in range(0,nVertice):
+		nome = input("Digite o nome do vertice: ")
+		nome = Vertex(nome,'NULL','NULL')
+		graph[nome] = aux
+		
+	for x in graph:
+		aux = {}
+		nArestas = int(input("Número de arestas de "+x.name+": "))	
+		for y in range(0,nArestas):
+			auxNome = input("Digite os adjacentes de "+x.name+": ")
+			for k in graph:
+				if auxNome == k.name:
+					auxNome = k
+			peso= int(input("Digite o peso: "))
+			aux[auxNome] = peso	
+		graph[x] = aux
+
+	return graph
 
 
-graph = {a:{b:10,c:3},b:{d:2,c:1},c:{b:4,d:8},d:{e:7},e:{d:9}}
 
 def Inicialize (graph,s):
 	for x in graph:
@@ -64,22 +87,38 @@ def BellmanFord(graph,s):
 				if(j.dist>i.dist+graph[i][j]):
 					return False
 			return True
-				
-				
-
-		
+	
 def caminhoMinimo(v):
 	print(v.name)
 	if(v.pred!=None):
 		caminhoMinimo(v.pred)
 
-BellmanFord(graph,a)
-if BellmanFord:
-	print("NÃO TEM CICLO NEGATIVO")
-else:
-	print("Tem ciclo negativo")
+def escolheAlgoritimo(graph):
+	print("1 - dijkstra")
+	print("2 - BellmanFord")
+	n = int(input("Digite qual Algoritimo usar: "))
+	if n == 1:
+		s = input("Digite o vértice inicial: ")
+		for k in graph:
+			if s == k.name:
+				s = k
+		dijkstra(graph,s)
+		v = input("Digite o vértice final: ")
+		for k in graph:
+			if v == k.name:
+				v = k
+		caminhoMinimo(v)
+	if n == 2:
+		s = input("Digite o vértice inicial: ")
+		for k in graph:
+			if s == k.name:
+				s = k
+		BellmanFord(graph,s)
+		v = input("Digite o vértice final: ")
+		for k in graph:
+			if v == k.name:
+				v = k
+		caminhoMinimo(v)
 
-print("Caminho mínime até o vértice: "+e.name)
-caminhoMinimo(e)
 
-# floyd-warshal
+escolheAlgoritimo(criaVertice())
---
+++ Caminhos Minimos trabalho/floydWarhsal.py
@@ -0,0 +1,45 @@
+inf=9999999999
+graph1=[ [0,10,3,inf,inf],[inf,0,1,2,inf],[inf,4,0,8,inf],[inf,inf,inf,0,7],[inf,inf,inf,9,0]]
+def criaVertice ():
+	graph=[]
+	nVertice = int(input("Digite o número de vértices: "))
+	for x in range(0,nVertice):
+		aux = []
+		for  y in range(0,nVertice):
+			if(x==y):
+				aux.append(0)
+			else:
+				c = input("Existe arestas de "+str(x)+" e "+str(y)+"(s/n): ")
+				if c == 'n':
+					aux.append(inf)
+				else:
+					peso = int(input("Digite o peso de "+str(x)+" e "+str(y)+": "))
+					aux.append(peso)
+		graph.append(aux)
+	return graph
+
+
+def FloydWarshal(graph):
+	n = len(graph)
+	
+	pred = [] 
+	for x in range(0,n):
+		aux=[]
+		for y in range(0,n):
+			aux.append(x)
+		pred.append(aux)			
+			
+	for k in range(0,n):
+		for i in range(0,n):
+			for j in range(0,n):
+				if graph[i][j]> graph[i][k]+graph[k][j]:	
+					graph[i][j]=graph[i][k]+graph[k][j]
+					pred[i][j]=pred[k][j]
+	print("Matriz de predecessor")				
+	print(pred)
+	print("Matriz de caminho mínimo")
+	print(graph)
+	return graph	
+
+
+FloydWarshal(criaVertice())			
